1d23eac671b2ec8e8aa98b16d6653874f4b96f73
xuejianyong <xuejianyong@hotmail.com>
2020-01-10 12:42:07 +0100

    the tutorial

---
+++ tutorial.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+var http = require("http");
+http.createServer(function (request, response){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.end('hello world\n');
+}).listen(8888);
+
+console.log('server running at http://127.0.0.1：:8888');
+
+var express = require('express');
